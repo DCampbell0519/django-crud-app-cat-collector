1ccf313330af2ebe7e9b090d2ffe59181a175562
Donovan Campbell <donovan@dlcentertainment.net>
2025-10-06 21:02:15 -0700

    django models complete

--- main_app/urls.py
+++ main_app/urls.py
@@ -4,4 +4,5 @@ from  . import views # views are similar to controllers
 urlpatterns = [
     path('', views.home, name='home'),
     path('about/', views.about, name='about'),
+    path('cats/', views.cats_index, name='cat-index'),
 ]
